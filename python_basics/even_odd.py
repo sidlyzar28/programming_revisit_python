64ade885b0a27edc645b313e89867bc12809b31a
Siddharth Chakraverty <siddharthchakraverty@gmail.com>
2026-09-08 20:11:27 +0530

    new program: length of string without len functiom

--- python_basics/even_odd.py
+++ python_basics/even_odd.py
@@ -1,6 +1,6 @@
 n=int(input("Enter a number: "))
 if n%2==0:
-    print(n,"is an even number")
+    print(n,":is an even number")
 else:
-    print(n,"is an odd number")
+    print(n,":is an odd number")
     
---
+++ python_basics/string_length.py
@@ -0,0 +1,5 @@
+s=input("Enter a string:")
+count=0
+for char in s:
+    count+=1
+print("Length of the string is:",count)
