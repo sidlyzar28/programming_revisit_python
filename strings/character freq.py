1539d9d1323f1828fee686ebacb063e3a98a21f2
Siddharth Chakraverty <siddharthchakraverty@gmail.com>
2026-09-09 01:33:24 +0530

    character frequency in a string

---
+++ strings/character freq.py
@@ -0,0 +1,9 @@
+text=input("Enter a string: ")
+freq={}
+for char in text:
+    if char in freq:
+        freq[char]+=1
+    else:
+        freq[char]=1
+for key,value in freq.items():
+    print(key,value)
