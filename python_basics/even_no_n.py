ff528e870b7687c4f3b12bd697c1ac4fa1a4e446
Siddharth Chakraverty <siddharthchakraverty@gmail.com>
2026-09-08 20:28:20 +0530

    added 2 possible ways to do the same problem

--- python_basics/even_no_n.py
+++ python_basics/even_no_n.py
@@ -1,3 +1,9 @@
 n=int(input("Enter a number: "))
 for i in range (0,n+1,2):
-    print(i)
+   print(i)
+
+
+#for printing in a single line  
+#n=int(input("Enter a number: "))
+#even_no=range(0,n+1,2)
+#print(*even_no)
