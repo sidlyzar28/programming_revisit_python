eb6b26005dcb31483cc2d9c0ea91809430dd6458
Siddharth Chakraverty <siddharthchakraverty@gmail.com>
2026-09-12 16:10:54 +0530

    all duplicate element

---
+++ strings/duplicate element.py
@@ -0,0 +1,8 @@
+n=int(input("Enter the number of elements in the list: "))
+arr=list(map(int,input().split()))
+freq={}
+for num in arr:
+    freq[num]=freq.get(num,0)+1
+for key in freq:
+    if freq[key]>1:
+        print(key,end=" ")
