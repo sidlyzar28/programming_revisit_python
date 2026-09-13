7d3b5c3847ecdde7c1c1779bc3d35da0725d83b5
Siddharth Chakraverty <siddharthchakraverty@gmail.com>
2026-09-13 13:09:58 +0530

    larger of 2 numbers

---
+++ python_basics/larger of 2.py
@@ -0,0 +1,8 @@
+n=int(input("enter 1st number:"))
+b=int(input("enter 2nd number:"))
+if n>b:
+    print("the larger number is:",n)
+elif b>n:
+    print("the larger number is:",b)
+else:
+    print("both numbers are equal")
