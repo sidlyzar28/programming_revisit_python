e21d7f414fb931bb237b5c0107ff20cd6be74906
Siddharth Chakraverty <siddharthchakraverty@gmail.com>
2026-09-13 21:18:43 +0530

    2nd largest didstinct element in the array

---
+++ arrays/2nd largest distinct element.py
@@ -0,0 +1,10 @@
+nn=int(input())
+arr=list(map(int,input().split()))
+largest=second=None
+for num in arr:
+    if largest is None or num>largest:
+        second=largest
+        largest=num
+    elif num!=largest and (second is None or num>second):
+        second=num
+print("The second largest distinct element in the list is:",second)
