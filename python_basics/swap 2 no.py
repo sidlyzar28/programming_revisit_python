c376da3389b46f30159ab3fbc0a44a6b6e110991
Siddharth Chakraverty <siddharthchakraverty@gmail.com>
2026-09-15 12:43:29 +0530

    swapping 2 nos using 3rd variable and wihout third varable

---
+++ python_basics/swap 2 no.py
@@ -0,0 +1,13 @@
+a=int(input())
+b=int(input())
+a=a+b
+b=a-b
+a=a-b
+print("After swapping: a =",a,"b =",b)
+##USING 3rd variable
+a=int(input())
+b=int(input())
+temp=a
+a=b
+b=temp
+print("After swapping: a =",a,"b =",b)
